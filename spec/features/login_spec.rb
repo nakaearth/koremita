6701fe6e1da1808da96640c18c7d3327a11f4993
nakamura shinichirou <naka5313@gmail.com>
2014-06-17 14:53:21 +0900

    capybaraで作ったテスト。。。時々とおる

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,12 +5,12 @@ describe 'ユーザがfacebookログインする', :type => :feature do
   context 'トップページに遷移してfacebookログインボタンをクリックした時' do
     context 'ログインに成功した場合' do
       before do
-        request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
-         visit root_path
+#        request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
+        visit root_path
         click_link 'Facebookでログイン'
       end
       it 'トップページに遷移する' do
-        expect(page.current_path).to eq '/auth/developer/callback'
+        expect(page.current_path).to eq '/facebook/login'
       end
     end
   end
