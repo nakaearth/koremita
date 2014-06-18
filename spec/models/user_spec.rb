f74d4fa928464b38349c7b90299d709a9050e9c5
nakamura shinichirou <naka5313@gmail.com>
2014-06-18 13:11:59 +0900

    フォーマット調整

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,27 +16,27 @@
 require 'spec_helper'
 
 describe User do
- let!(:current_user) { create(:current_user) }
- let!(:auth_provider) { create(:auth_provider, user: current_user) }
- let!(:test_movie1) { create(:test_movie, user: current_user) }
- let!(:test_movie2) { create(:test_movie, user: current_user) }
- let!(:test_movie3) { create(:test_movie, user: current_user) }
- let!(:test_movie4) { create(:test_movie, user: current_user) }
+  let!(:current_user) { create(:current_user) }
+  let!(:auth_provider) { create(:auth_provider, user: current_user) }
+  let!(:test_movie1) { create(:test_movie, user: current_user) }
+  let!(:test_movie2) { create(:test_movie, user: current_user) }
+  let!(:test_movie3) { create(:test_movie, user: current_user) }
+  let!(:test_movie4) { create(:test_movie, user: current_user) }
 
   # モデルの定義をチェック
- it { expect have_many(:movies) }
- it { expect validate_presence_of(:email) }
- it { expect validate_presence_of(:name) }
- it { expect(current_user.movies.size).to eql(4) }
+  it { expect have_many(:movies) }
+  it { expect validate_presence_of(:email) }
+  it { expect validate_presence_of(:name) }
+  it { expect(current_user.movies.size).to eql(4) }
 
- describe "user モデル属性チェック" do
-   context "ログイン後" do
-     before do
-       @user = current_user
-     end
-     it "email address" do
-       expect(@user.email).to eql('test@gmail.com')
-     end
-   end
- end
+  describe "user モデル属性チェック" do
+    context "ログイン後" do
+      before do
+        @user = current_user
+      end
+      it "email address" do
+        expect(@user.email).to eql('test@gmail.com')
+      end
+    end
+  end
 end
