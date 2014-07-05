61c1d1c3df19ac04883cefe106445f9e9c45a02a
nakamura shinichirou <naka5313@gmail.com>
2014-07-05 22:44:32 +0900

    rubocop対応

--- app/service/movie_register.rb
+++ app/service/movie_register.rb
@@ -14,20 +14,18 @@ class MovieRegister
       end
 
       movie = @user.movies.build(title: movie_params[:title],
-                        description: movie_params[:description],
-                        rate: movie_params[:rate])
-      
+                                 description: movie_params[:description],
+                                 rate: movie_params[:rate])
+
       movie.photo = FileUploader.upload_cloudinary(movie_params[:photo], 'movie',
-                                   movie.title + Time.zone.now.strftime('%Y%m%d%H%M'))
-                                 
+                                                   movie.title + Time.zone.now.strftime('%Y%m%d%H%M'))
+
       # movieテーブルに登録
       movie.save!
 
       unless youtub_params.blank?
- #       youtub = Youtub.new(title: youtub_params[:title],
- #                           url: youtub_params[:url])
         youtub = movie.build_youtub(title: youtub_params[:title],
-                            url: youtub_params[:url])
+                                    url: youtub_params[:url])
         youtub.save!
       end
 
