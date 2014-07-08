b6cf310ccce44dd963d61f9bc2a24869a527a23d
nakamura shinichirou <naka5313@gmail.com>
2014-07-08 12:22:50 +0900

    udpate files

--- lib/api/itunes_search.rb
+++ lib/api/itunes_search.rb
@@ -2,14 +2,14 @@ require 'itunes-search-api'
 
 module Api
   class ItunesSearch
-    def initialize(term, country , category)
+    def initialize(term, country, category)
       @term     = term
       @country  = country
       @category = category
     end
 
     def search
-      ITunesSearchAPI.search(term: @term , country:  @country, media:  @category)
+      ITunesSearchAPI.search(term: @term, country:  @country, media:  @category)
     end
   end
 end
