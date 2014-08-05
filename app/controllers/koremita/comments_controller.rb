3b23b6825318b94883f10d431de9b4beee806af5
nakamura shinichirou <naka5313@gmail.com>
2014-08-05 18:17:41 +0900

    controllerの中身を少し修正

--- app/controllers/koremita/comments_controller.rb
+++ app/controllers/koremita/comments_controller.rb
@@ -1,38 +1,36 @@
 module Koremita
-  module Movie
-    class CommentsController < ApplicationController
-      before_action :set_comment
+  class CommentsController < ApplicationController
+    before_action :set_comment
 
-      def index
-        @comments = @movie.comments.page(params[:page]).per(20)
-      end
+    def index
+      @comments = @movie.comments.page(params[:page]).per(20)
+    end
 
-      def show
-      end
+    def show
+    end
 
-      def new
-        @comment = Comment.new
-      end
+    def new
+      @comment = Comment.new
+    end
 
-      def create
-        @comment = Comment.new
-      end
+    def create
+      @comment = Comment.new
+    end
 
-      def edit
-        authorize @comment
-      end
+    def edit
+      authorize @comment
+    end
 
-      def update
-        authorize @comment
-      end
+    def update
+      authorize @comment
+    end
 
-      def set_movie
-        @movie = Movie.find(params[:movie_id])
-      end
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
 
-      def set_comment
-        @comment = Comment.find(params[:id])
-      end
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
   end
 end
