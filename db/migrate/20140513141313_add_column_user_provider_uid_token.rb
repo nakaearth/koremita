65f389b196f410df55680c5c7dde13c4eee02062
nakamura shinichirou <naka5313@gmail.com>
2014-08-27 23:07:50 +0900

    マイグレーションファイル修正

--- db/migrate/20140513141313_add_column_user_provider_uid_token.rb
+++ db/migrate/20140513141313_add_column_user_provider_uid_token.rb
@@ -3,6 +3,6 @@ class AddColumnUserProviderUidToken < ActiveRecord::Migration
     add_column :users, :provider, :string, null: false, default: ""
     add_column :users, :uid, :string, null: false, length: 256, default: ""
     add_column :users, :name, :string, null: false, length: 30 , default: ""
-    add_column :users, :token, :strging, null: false, default: ""
+    add_column :users, :token, :string, null: false, default: ""
   end
 end
