6a91a82c5ec9fed5f8c723f53ac62c95fc539f63
nakamura shinichirou <naka5313@gmail.com>
2014-09-03 02:05:02 +0900

    api用のcontrollerを追加

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,16 @@
+class Api::ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  include Pundit
+  helper_method :current_user
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def current_user
+  end
+
+  def render_404
+    render json { status: ng }, status: :not_found
+  end
+
+end
---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,7 @@
+module api
+  class MoviesController < ApplicationController
+    def index
+      
+    end
+  end
+end
