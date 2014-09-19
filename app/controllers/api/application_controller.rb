6d65092cd9fa8abe8830602b16635982b3d45633
nakamura shinichirou <naka5313@gmail.com>
2014-09-19 13:20:30 +0900

    apiの一部作成中

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,13 +1,13 @@
 module Api
   class ApplicationController < ActionController::Base
     include Pundit
-  
+
     before_action :login?
     helper_method :current_user
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
-   
+
     def current_user
       api = ApiToken.find_by(access_token: params[:access_token])
       @current_use ||= api.user
--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,11 +2,14 @@ module Api
   class MoviesController < ApplicationController
     def index
       # 映画一覧を返す。同じように友達の映画も返すように別途APIを作成する
-      frineds = FacebookGraph.new(current_user.facebook_provider.access_token).current_user_friends
-      # frinedsからランダムでIDを取得しそれを元にUser.include(:auth_provoder).find_by(uid: uid)
-      # 見たいにしてユーザを取得。その後持っている映画情報3件程を取得し返すようにする
-      
-      @movies = current_user.movies.limit(3)
+      friends = FacebookGraph.new(current_user.facebook_provider.access_token).current_user_friends
+      friends.shuffle!
+      @movies = []
+      friends[0..3].each do |friend|
+        user = User.include(:auth_provider).find_by(uid: friend.user.uid)
+        @movies <<  user.my_movies
+      end
+      @movies
     end
   end
 end
